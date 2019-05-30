926263cf83749dd56b9fd36dcd2a4de3003a1b37
Andrew Donelson <admin@nlaak.com>
2019-05-30 18:46:10 -0500

    - added docs to datacenter - create base cluster model

--- models/datacenter.go
+++ models/datacenter.go
@@ -8,9 +8,12 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// TestDataFile is the file to be used for testing
 const TestDataFile = "./data/dc.yml"
 
+// Datacenter represents a datacenter with all its clusters, vm's etc
 type Datacenter struct {
+	// TODO: Convert YAML to JSON, Create Struct from JSON, Load YAML
 }
 
 // NewDataCenter given a valid YML Datacenter configuration file
@@ -18,20 +21,24 @@ type Datacenter struct {
 func NewDataCenter(dataFile string) (*Datacenter, error) {
 	dc := Datacenter{}
 
+	// file exists & readable?
 	_, err := os.Stat(dataFile)
 	if err != nil {
 		return nil, err
 	}
 
+	// load the file
 	data, err := ioutil.ReadFile(dataFile)
 	if err != nil {
 		return nil, err
 	}
 
+	// decode the file as yaml
 	yerr := yaml.Unmarshal(data, dc)
 	if yerr != nil {
 		return nil, yerr
 	}
 
+	// return valid pointer datacenter object
 	return &dc, nil
 }
